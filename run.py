91d6eb5d607b9ba296ab14d5b61fcb2c501cf225
LinnSilver <baralinn@gmail.com>
2023-02-15 10:37:01 +0000

    Add funktion to whrite to json file

--- run.py
+++ run.py
@@ -1,25 +1,40 @@
 import json
 
-answer = None
+def run():
+    # Opening JSON file
+    f = open('data.json', 'r')
 
-while answer not in ("c", "p"):
-    print("c= Create a quiz?")
-    print("p= Play a quiz?")
-    answer = input("Please choose:\n")
+    # Load JSON data to memory
+    try:
+        data = json.load(f)
+        if type(data) is not list:
+            data = []
+        f.close()
+    except:
+        print("Failed to read data from JSON")
+        data = []
 
-    if answer == "c":
-        print("ask_for_question")
-        question = ask_for_question()
-        answer_for_question = ask_for_answers()
-        quiz_object = {"question": question, "ansers_for_question": answer_for_question}
-        print(str(quiz_object))
-        f = open('data.json', 'w')
-        data.append(quiz_object)
-        json.dump(data, f)
-    elif answer == "p":
-        print("Play")
-    else:
-        print("Please enter c or p.")
+    answer = None
+
+    while answer not in ("c", "p"):
+        print("c= Create a quiz?")
+        print("p= Play a quiz?")
+        answer = input("Please choose:\n")
+
+        if answer == "c":
+            print("ask_for_question")
+            question = ask_for_question()
+            answer_for_question = ask_for_answers()
+            quiz_object = {"question": question, "answers_for_question": answer_for_question}
+            print(str(quiz_object))
+            f = open('data.json', 'w')
+            data.append(quiz_object)
+            json.dump(data , f)
+            f.close()
+        elif answer == "p":
+            print("Play")
+        else:
+            print("Please enter c or p.")
 
 def ask_for_question():
     """ Ask user for question """
@@ -46,7 +61,7 @@ def ask_for_answers():
     corect_answer_index = answer_list.index(corect_answer)
     if corect_answer_index >= 0:
         del answer_list[corect_answer_index]
-        return {"answer":answers_list, "corect_answer": corect_answer}
+        return {"answer":answer_list, "corect_answer": corect_answer}
 
 
 if __name__ == '__main__':
