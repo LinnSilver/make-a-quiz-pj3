73cf40d80dff7d185c393e40f9c64e8049d30d8a
LinnSilver <baralinn@gmail.com>
2023-02-15 09:58:58 +0000

    Add funtion to ask for question

--- run.py
+++ run.py
@@ -7,13 +7,12 @@ while answer not in ("c", "p"):
     print("p= Play a quiz?")
     answer = input("Please choose:\n")
 
-
     if answer == "c":
         print("ask_for_question")
         question = ask_for_question()
         answer_for_question = ask_for_answers()
-        quiz_object = {"question": question, "ansers_for_uestion": answer_for_question}
-        print(str()(quiz_object))
+        quiz_object = {"question": question, "ansers_for_question": answer_for_question}
+        print(str(quiz_object))
         f = open('data.json', 'w')
         data.append(quiz_object)
         json.dump(data, f)
@@ -22,3 +21,12 @@ while answer not in ("c", "p"):
     else:
         print("Please enter c or p.")
 
+def ask_for_question():
+    """ Ask user for question """
+    while True:
+        print("Please enter question\n")
+        data_question = input("Question is:")
+
+        question = data_question
+
+        return question
