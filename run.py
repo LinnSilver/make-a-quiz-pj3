490d49d54629a3d9bc2493625ae72d0031da3974
LinnSilver <baralinn@gmail.com>
2023-02-15 10:17:11 +0000

    Add funtion to ask for answers

--- run.py
+++ run.py
@@ -30,3 +30,24 @@ def ask_for_question():
         question = data_question
 
         return question
+
+def ask_for_answers():
+    number_of_answers = int(input("How many answers do you want to add?:"))
+
+    i = 0
+    answer_list = []
+    while i < number_of_answers:
+        print("Please enter answer\n")
+        data_answer = input("Answer:")
+        answer_list.append(data_answer)
+        i += 1
+    print("Wath is corect answer?")
+    corect_answer = input()
+    corect_answer_index = answer_list.index(corect_answer)
+    if corect_answer_index >= 0:
+        del answer_list[corect_answer_index]
+        return {"answer":answers_list, "corect_answer": corect_answer}
+
+
+if __name__ == '__main__':
+    run()
